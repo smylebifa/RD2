1ccaae8130384600dfffddd5b3bc7229cff83043
smylebifa <samoilovilia@mail.ru>
2023-02-10 18:07:33 +0300

    add changes in stages

--- RD/Services/StagesService .cs
+++ RD/Services/StagesService .cs
@@ -21,6 +21,9 @@ namespace RD.Services
         private static readonly List<Stage> Stages = new List<Stage>
         {
                 new Stage(1, "Этап 1"),
+                new Stage(2, "Этап 2"),
+                new Stage(3, "Этап 3"),
+                new Stage(4, "Этап 4"),
         };
 
         public IEnumerable<Stage> GetStages()
