459433d5f124799d521ca33c0ce05a8240e90675
smylebifa <samoilovilia@mail.ru>
2023-04-29 19:48:28 +0300

    correction in code asp controllers

--- RD/Controllers/StagesController.cs
+++ RD/Controllers/StagesController.cs
@@ -1,12 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using RD.Models;
 using RD.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using RD.Models;
 
 namespace RD.Controllers
 {
